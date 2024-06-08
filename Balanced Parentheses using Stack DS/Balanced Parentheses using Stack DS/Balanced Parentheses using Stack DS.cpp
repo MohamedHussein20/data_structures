d4e3c0b29ef368bd28beabca3ba3b7e11a21b780
Mohamed Hussein <mohussein1279@gmail.com>
2024-06-08 13:18:47 +0300

    update

--- Balanced Parentheses using Stack DS/Balanced Parentheses using Stack DS/Balanced Parentheses using Stack DS.cpp
+++ Balanced Parentheses using Stack DS/Balanced Parentheses using Stack DS/Balanced Parentheses using Stack DS.cpp
@@ -2,58 +2,70 @@
 #include<stack>
 #include<string>
 using namespace std; 
-bool arePair(char open, char close)
-{
-	if (open=='(' && close==')')
-	{
-		return true;
-	}
+bool isValid(string s) {
+        stack<char> st ;
+        for (int i = 0 ;  i< s.length() ; i++)
+        {
+            char ch = s[i];
 
-	else if (open == '{' && close == '}')
-	{
-		return true;
-	}
 
-	else if (open == '[' && close == ']')
-	{
-		return true;
-	}
-	return false;
-}
-bool areBalanced(string exp)
-{
-	stack<char>s;
-	for (int i = 0; i < exp.size(); i++)
-	{
-		if (exp[i]=='(' || exp[i]=='{' || exp[i] == '[')
-		{
-			s.push(exp[i]);
-		}
-		else if ((exp[i] == ')' || exp[i] == '}' || exp[i] == ']'))
-		{
-			if ( s.empty() || arePair( s.top() , exp[i] ) )
-			{
-				return false;
-			}
-			else
-			{
-				s.pop();
-			}
-		}
-		return s.empty();
-	}
+            if (ch == '(' || ch == '{' || ch == '[')
+            {
+                st.push(ch) ;
+            }
+
+            else {
+
+                if (!st.empty())
+                {
+                    char top = st.top() ;
+                    if ((ch == ')' && top == '(') ||
+                        (ch == '}' && top == '{') ||
+                        (ch == ']' && top == '['))
+                        {
+
+                            st.pop() ;
+                        }
+                        else
+                        {
+                            return false ;
+                        }
+                }
+                else
+                {
+
+                    return false ;
+                }
+            }
+        }
+
+
+        if (st.empty())
+        {
+            return true ;
+        }
+        return false ;
+    }
+
+
+    if (st.empty())
+    {
+        return true;
+    }
+    return false;
+  }
 }
 int main()
 {
 	string s;
 	cin >> s;
-	if (areBalanced(s))
+	if (isValid(s))
 	{
 		cout << "Balanced" << endl;
 	}
 	else
 	{
-		cout << "Balanced" << endl;
+		cout << "Not Balanced" << endl;
 	}
 	return 0;
 }
